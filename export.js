e39b1689f52ac440dcba64990410172e4f4dd451
Victor Borisov <refactorman@gmail.com>
2017-07-31 09:42:23 +0900

    added ABI and Binary exporter

---
+++ export.js
@@ -0,0 +1,5 @@
+var m = require("./build/contracts/MichCoin");
+console.log("==============ABI==============");
+console.log(m.abi);
+console.log("==============BINARY==============");
+console.log(m.unlinked_binary);
