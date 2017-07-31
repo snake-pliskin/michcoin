286600aaf87a09ff7c89b45e9d0ad1cd04b849bc
Victor Borisov <refactorman@gmail.com>
2017-07-31 17:30:17 +0900

    fixed ABI export

--- export.js
+++ export.js
@@ -1,5 +1,6 @@
 var m = require("./build/contracts/MichCoin");
 console.log("==============ABI==============");
-console.log(m.abi);
+var s = JSON.stringify(m.abi, null, 0);
+console.log(s);
 console.log("==============BINARY==============");
 console.log(m.unlinked_binary);
