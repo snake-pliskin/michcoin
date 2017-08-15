495aa85be3280ae9d7ba2e0e65202ba13fba3888
Victor Borisov <refactorman@gmail.com>
2017-08-15 14:26:08 +0900

    delete unused

--- export.js
+++
@@ -1,6 +0,0 @@
-var m = require("./build/contracts/MichCoin");
-console.log("==============ABI==============");
-var s = JSON.stringify(m.abi, null, 0);
-console.log(s);
-console.log("==============BINARY==============");
-console.log(m.unlinked_binary);
